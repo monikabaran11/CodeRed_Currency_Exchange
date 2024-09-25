6183ed1ee685872e9a1e6879c17abf841598a411
monikabaran11 <162638600+monikabaran11@users.noreply.github.com>
2024-09-25 22:23:58 +0200

    test

--- FX project.py
+++ FX project.py
@@ -1,1 +1,34 @@
 #test
+# requirments: You are on a vacation in a foreign country and you need to exchange some currency to pay for your expenses.
+#
+# You have all the skills which are needed to create an application (maybe even the web one).
+#
+# Excited to try it out you open your laptop and start developing a currency exchange application.
+#
+#
+#
+# Requirements
+#
+# - retrieve a current rate for at least 5 currencies (ideally for every available currency)
+#
+# - convert an amount provided by user from one currency to another
+#
+# - based on current and historic data get the information about the trend of the rate and suggest if it is a good moment for an exchange of a given currency (or currencies)
+#
+# - allow user for creation of a stop order (an order to buy or sell a currency once the price reaches a specific price, known as the stop price.
+#
+#                 When the stop price is reached, a stop order becomes a market order and is executed at the best available price (which can be lower or higher than the stop price).
+#
+#                 Stop orders aren't guaranteed to execute until the price of the currency reaches the set stop price.)
+#
+# - allow user for creation of a limit order (an order to buy or sell currency at a price you choose, or better. Buy limit orders are executed at your specified price or lower,
+#
+#                 sell limit orders are executed at your specified price or higher. Limit orders aren't guaranteed – they'll only exchange if the price of a currency increases or decreases to your set limit price.
+#
+#                 If it doesn't, your order is left unfilled.)
+#
+# - data retrieved from external sources should be stored in a csv file (or database)
+#
+# - plot a chart showing rates across specific time frame.
+#
+# - create a chart showing rates change across day-to-day, month-to-month and year-to-year for one or more currencies
