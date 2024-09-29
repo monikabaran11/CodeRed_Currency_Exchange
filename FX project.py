053fa77c2017b8bb447eb67ba80ff2c07d3c772a
monikabaran11 <162638600+monikabaran11@users.noreply.github.com>
2024-09-29 23:05:53 +0200

    Add files via upload
    added simply ccy calculator

--- FX project.py
+++ FX project.py
@@ -1,34 +1,44 @@
-#test
-# requirments: You are on a vacation in a foreign country and you need to exchange some currency to pay for your expenses.
-#
-# You have all the skills which are needed to create an application (maybe even the web one).
-#
-# Excited to try it out you open your laptop and start developing a currency exchange application.
-#
-#
-#
-# Requirements
-#
-# - retrieve a current rate for at least 5 currencies (ideally for every available currency)
-#
-# - convert an amount provided by user from one currency to another
-#
-# - based on current and historic data get the information about the trend of the rate and suggest if it is a good moment for an exchange of a given currency (or currencies)
-#
-# - allow user for creation of a stop order (an order to buy or sell a currency once the price reaches a specific price, known as the stop price.
-#
-#                 When the stop price is reached, a stop order becomes a market order and is executed at the best available price (which can be lower or higher than the stop price).
-#
-#                 Stop orders aren't guaranteed to execute until the price of the currency reaches the set stop price.)
-#
-# - allow user for creation of a limit order (an order to buy or sell currency at a price you choose, or better. Buy limit orders are executed at your specified price or lower,
-#
-#                 sell limit orders are executed at your specified price or higher. Limit orders aren't guaranteed – they'll only exchange if the price of a currency increases or decreases to your set limit price.
-#
-#                 If it doesn't, your order is left unfilled.)
-#
-# - data retrieved from external sources should be stored in a csv file (or database)
-#
-# - plot a chart showing rates across specific time frame.
-#
-# - create a chart showing rates change across day-to-day, month-to-month and year-to-year for one or more currencies
+
+buy_currency =input("Currency you would like to buy (EUR,GBP,PLN,USD):").upper()
+buy_amount =int(input("Amount:"))
+sell_currency = input("Currency you would to pay (EUR,GBP,PLN,USD):").upper()
+eur_to_pln_rate = 4.27
+eur_to_gbp_rate = 0.83
+eur_to_usd_rate = 1.12
+
+def from_buy_to_sell():
+    if buy_currency == "EUR":
+        if sell_currency == "PLN":
+            sell_amount = buy_amount * eur_to_pln_rate
+        elif sell_currency == "GBP":
+            sell_amount = buy_amount * eur_to_gbp_rate
+        elif sell_currency == "USD":
+            sell_amount = buy_amount * eur_to_usd_rate
+    elif buy_currency == "GBP":
+       if sell_currency == "PLN":
+           sell_amount= buy_amount*(1/eur_to_gbp_rate)*eur_to_pln_rate
+       elif sell_currency == "USD":
+           sell_amount= buy_amount*(1/eur_to_gbp_rate)*eur_to_usd_rate
+       elif sell_currency == "EUR":
+           sell_amount = buy_amount*(1/eur_to_gbp_rate)
+    elif buy_currency == "USD":
+        if sell_currency == "PLN":
+            sell_amount = buy_amount * (1 / eur_to_usd_rate) * eur_to_pln_rate
+        elif sell_currency == "GBP":
+            sell_amount = buy_amount * (1 / eur_to_usd_rate) * eur_to_gbp_rate
+        elif sell_currency == "EUR":
+            sell_amount = buy_amount * (1 / eur_to_usd_rate)
+    elif buy_currency == "PLN":
+        if sell_currency == "USD":
+            sell_amount = buy_amount * (1 / eur_to_pln_rate) * eur_to_usd_rate
+        elif sell_currency == "GBP":
+            sell_amount = buy_amount * (1 / eur_to_pln_rate) * eur_to_gbp_rate
+        elif sell_currency == "EUR":
+            sell_amount = buy_amount * (1 / eur_to_pln_rate)
+    else:
+
+        print("Incorrect input, please try once again")
+    return print(f"To buy {buy_amount}{buy_currency} you will need to pay {sell_amount:.2f}{sell_currency}.")
+
+
+from_buy_to_sell()
