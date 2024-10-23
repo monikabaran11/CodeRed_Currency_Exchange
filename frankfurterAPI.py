02dcec6d9db12b12126ee4c6943d1aeb49582884
monikabaran11 <162638600+monikabaran11@users.noreply.github.com>
2024-10-23 23:02:46 +0200

    Add files via upload

--- frankfurterAPI.py
+++ frankfurterAPI.py
@@ -15,12 +15,12 @@ def download_conversion_rates():
     else:
         return print(f"Error: Unable to fetch data (status code: {response.status_code})")
 
-def download_rates_to_csv(): # not used yet but might be used for future code.
-    with open('exchange_rates.csv', mode="w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Currency', 'Rate'])
-        for currency, rate in conversion_rates.items():
-            writer.writerow([currency,rate])
+# def download_rates_to_csv(): # not used yet but might be used for future code.
+#     with open('exchange_rates.csv', mode="w", newline="") as csvfile:
+#         writer = csv.writer(csvfile)
+#         writer.writerow(['Currency', 'Rate'])
+#         for currency, rate in conversion_rates.items():
+#             writer.writerow([currency,rate])
 def from_buy_to_sell(buy_currency,buy_amount,pay_currency):
 
     if buy_currency in conversion_rates and pay_currency in conversion_rates:
@@ -38,9 +38,9 @@ def from_sell_to_buy(sell_currency, sell_amount, receive_currency):
 
 
 def historical_rates_for_plot (base_currency,plot_currency): #base_currency=from_currency,plot_currency =to_currency(quote currency),start_date= set for 6 month
-    start_date = (datetime.now() - relativedelta(months=3)).date()
+    start_date = (datetime.now() - relativedelta(months=6)).date()
     start_date_str = str(start_date)
-    print(f'start date {start_date_str}')
+    print(f'Starting Point: {start_date_str}')
     url = f"https://api.frankfurter.app/{start_date_str}..?base={base_currency}&symbols={plot_currency}"
     response = requests.get(url)
 
@@ -54,17 +54,42 @@ def historical_rates_for_plot (base_currency,plot_currency): #base_currency=from
             dates.append(date)
             rate.append(rates[plot_currency])
         df = pd.DataFrame({'Date': pd.to_datetime(dates), 'Rate': rate})  # changing format to datetime
-        print(df.tail())
         return df
 
     else:
         return print(f"Error: Unable to fetch data (status code: {response.status_code})")
 
 def plot_historical_rates(base_currency,plot_currency,df):
-    fig = px.line(df, x='Date', y='Rate', title=f'Historical rates for {base_currency}',labels={'Rate': f'Exchange Rate in ({base_currency}/{plot_currency})'})
+    fig = px.line(df, x='Date', y='Rate', title=f'Historical rates for {base_currency}',labels={'Rate': f'Exchange Rate in ({base_currency}/{plot_currency})','Date': 'For last 6 months'})
     fig.add_hline(y=current_rate,line_color="Red",line_dash='dash', line_width=2, annotation_text=f'Current Rate({base_currency}/{plot_currency})', annotation_position="bottom right")
     fig.update_layout(showlegend=True)
-    fig.show()
+    return fig.show()
+def calculating_RSI(df_historical_rates):
+    df_historical_rates['Daily_change'] = (df_historical_rates['Rate'].diff()).fillna(0)
+    df_historical_rates['Positive'] = [x if x > 0 else 0 for x in df_historical_rates['Daily_change']]
+    df_historical_rates['Negative'] = [x if x < 0 else 0 for x in df_historical_rates['Daily_change']]
+    df_historical_rates['Positive_days'] = [1 if x > 0 else 0 for x in df_historical_rates['Positive']]
+    df_historical_rates['Negative_days'] = [1 if x < 0 else 0 for x in df_historical_rates['Negative']]
+    df_for_advise = df_historical_rates
+    sum_positive = df_for_advise['Positive'].sum()
+    sum_negative = -df_for_advise['Negative'].sum()
+    sum_positive_days = df_for_advise['Positive_days'].sum()
+    sum_negative_days = df_for_advise['Negative_days'].sum()
+    average_profit = sum_positive / sum_positive_days
+    average_loss = sum_negative / sum_negative_days
+    RS = average_profit / average_loss
+    RSI = 100 - (100 / (1 + RS))
+    print(f'RSI {RSI:.5f} RSI>70 good to Sell, RSI<30 good to Buy')
+    return RSI
+
+def calculating_average_min_max_exchange_rate (df_historical_rates):
+    average_ex_rate=df_historical_rates['Rate'].mean()
+    max_ex_rate=df_historical_rates['Rate'].max()
+    min_ex_rate=df_historical_rates['Rate'].min()
+    print(f'Average rate {average_ex_rate:0.5f}')
+    print(f'Highest rate{max_ex_rate}')
+    print(f'Lowest rate {min_ex_rate}')
+
 
 conversion_rates = download_conversion_rates()
 while True:
@@ -88,12 +113,15 @@ while True:
     else:
         print("Unrecognized choice of direction, please try once again")
 
-investment_advise =(input("Do you want to see investment advise for your currency exchange order?'Provide Y/N").upper())
+investment_advise = (input("Do you want to see investment advise for your currency exchange order?'Provide Y/N").upper())
+
 while True:
     if investment_advise == "Y":
         df_historical_rates = historical_rates_for_plot(from_currency,to_currency)
         current_rate = df_historical_rates['Rate'].iloc[-1]
-        print(f'Current rate {current_rate:3f} ({from_currency}/{to_currency})')
+        print(f'Current rate {current_rate:.5f} ({from_currency}/{to_currency})')
+        calculating_RSI(df_historical_rates)
+        calculating_average_min_max_exchange_rate(df_historical_rates)
         plot_historical_rates(from_currency,to_currency,df_historical_rates)
         break
     elif investment_advise == "N":
@@ -108,13 +136,3 @@ while True:
 
 # based on current and historic data get the information about the trend of the rate and suggest if it is a good moment for an exchange of a given currency (or currencies)
 # asking user if would like to see the trend plot for the pair of currencies chosen
-# def historical_rates_for_plot()#(buy currency,sell_currency,start_date,end_date)
-#     url = "https://api.frankfurter.app/latest"
-
-   # response = requests.get(url)
-#
-#     if response.status_code == 200:
-#         data = response.json()
-#
-# plot_choice=input("Woudl you like to see a plot of the chosen currency? Y/N").upper()
-# if plot_choice=="Y":
